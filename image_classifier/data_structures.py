85415792a2d011ad0f514ee38fef77ea00d745cd
Igor Sosnowicz <isosnowicz@internships.antmicro.com>
2025-08-07 13:47:02 +0200

    Reorder imports, write docstrings for data_structures module

--- image_classifier/data_structures.py
+++ image_classifier/data_structures.py
@@ -1,23 +1,40 @@
+"""The module with project-wide data structures, including: enumerations, named tuples, and data classes."""
+
 from enum import Enum
 from typing import NamedTuple, Optional
-
-import torch
 from dataclasses import dataclass
 from pathlib import Path
 
+import torch
+
 
 class ColourModel(Enum):
+    """
+    The enumeration mapping colour models for images to a corresponding
+    number of channels.
+    """
+
     GRAYSCALE = 1
     RGB = 3
     RGBA = 4
 
 
 class Set(NamedTuple):
+    """
+    The named tuple constituting set, a part of an entire dataset.
+    It consists of input features (X) and class labels (Y).
+    """
+
     features: torch.Tensor
     labels: torch.Tensor
 
 
 class Dataset(NamedTuple):
+    """
+    The named tuple representing a dataset containing test,
+    training (optionally), and validation (optionally) sets.
+    """
+
     test: Set
     training: Optional[Set] = None
     validation: Optional[Set] = None
@@ -25,6 +42,30 @@ class Dataset(NamedTuple):
 
 @dataclass
 class DataSourceDirectory:
+    """
+    Configuration for a directory containing image data.
+    Specifies the location, image format, colour model, class label,
+    and a message to display while loading. Used to define which
+    directories to load data from and how to interpret their contents.
+
+    Attributes
+    ----------
+    path: Path
+        A path to a directory with images.
+    colour_model: ColourModel
+        A colour model for all images in the directory.
+    class_label: int
+        A numeric value indicating, to which class a sample belongs.
+        In other words, it is the label of the sample.
+        1 means `fire` class, 0 means `nofire`.
+    loading_message: str
+        A text displayed by an interactive progress bar while
+        loading a directory.
+    extension: str
+        A file extension that should be used to load images from the directory.
+        For example, `jpg` or `png`.
+    """
+
     path: Path
     colour_model: ColourModel
     class_label: int
