be4bc378e3498cfa7e37961046fb99a2c736ae23
Igor Sosnowicz <isosnowicz@internships.antmicro.com>
2025-08-07 14:24:48 +0200

    Write docstrings for device module

--- image_classifier/device.py
+++ image_classifier/device.py
@@ -1,3 +1,8 @@
+"""
+The module providing an unified location to import a device,
+where the training and inference should be performed.
+"""
+
 import torch
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
