de34e92627d25408027a80b4fb439bf4b2a608eb
igor-sosnowicz <igor@sosnowicz.eu>
2025-08-08 00:33:55 +0200

    Add docstrings to evaluation module, parametrise showcasing and assessing images

--- image_classifier/evaluation.py
+++ image_classifier/evaluation.py
@@ -1,4 +1,9 @@
+"""
+The module for evaluation and showcasing the trained image classifier capabilities.
+"""
+
 from pathlib import Path
+from random import shuffle
 
 import torch
 import torchvision
@@ -6,6 +11,7 @@ from PIL import Image
 
 from image_classifier.device import device
 from image_classifier.data_structures import (
+    ColourModel,
     Set,
 )
 from image_classifier.metric import Metric
@@ -14,6 +20,19 @@ from image_classifier.metric import Metric
 def evaluate(
     model: torch.nn.Module, metric_types: list[type[Metric]], test_set: Set
 ) -> None:
+    """
+    Evaluate the trained model with Mean Squared Error (MSE) loss and other metrics.
+
+    Parameters
+    ----------
+    model : torch.nn.Module
+        The model to be evaluated.
+    metric_types : list[type[Metric]]
+        A list of classes representing metrics that should be calculated.
+        These metric classes should be imported from `images_classifier.metric` module.
+    test_set : Set
+        A test set, on which the metrics should be calculated.
+    """
     loss_fn = torch.nn.MSELoss()
     model.eval()
 
@@ -28,19 +47,36 @@ def evaluate(
             metric = Metric(actual=labels, predicted=predictions)
             print(f"{metric.get_metric_name()}: {metric.get_result()}")
 
-    # TODO: Implement the following metrics: confusion matrix, F1 score, precision, accuracy.
-
-    print(f"Total loss on the test set: {loss}")
+    print(f"Loss: {loss}")
 
 
 def assess_images(
-    model: torch.nn.Module, image_paths: list[Path], true_label: str
+    model: torch.nn.Module,
+    image_paths: list[Path],
+    true_label: str,
+    colour_model: ColourModel,
 ) -> None:
+    """
+    Assess the model manually by printing its predictions and actual labels for provided images.
+
+    All the images have to share the label and colour model.
+
+    Parameters
+    ----------
+    model : torch.nn.Module
+        The model to be evaluated.
+    image_paths : list[Path]
+        A list of paths to images.
+    true_label : str
+        A common label for all images.
+    colour_model : ColourModel
+        A common colour model for all images.
+    """
     transform = torchvision.transforms.Compose([torchvision.transforms.ToTensor()])
     model.eval()
 
     for img_path in image_paths:
-        img = Image.open(img_path).convert("RGB")
+        img = Image.open(img_path).convert(colour_model.name)
         img_tensor = transform(img).unsqueeze(0).to(device)
 
         with torch.inference_mode():
@@ -54,9 +90,32 @@ def assess_images(
 
 
 def showcase(
-    test_set_path: Path, model: torch.nn.Module, samples_per_class: int = 5
+    test_set_path: Path,
+    model: torch.nn.Module,
+    samples_per_class: int = 5,
+    randomise_order: bool = True,
+    colour_model: ColourModel = ColourModel.RGB,
 ) -> None:
-    # Load a few images from the test set and show their paths, infer their labels with the model.
+    """
+    Showcase the capabilities of a model by printing real labels
+    and model predictions for samples from a test set.
+
+    Parameters
+    ----------
+    test_set_path : Path
+        A path to a test set.
+    model : torch.nn.Module
+        The model to be evaluated.
+    samples_per_class : int, optional
+        A number of samples from each class to show, by default 5.
+    randomise_order : bool
+        Whether shown samples should chosen randomly.
+        If False, the samples are the first samples,
+        sorted by a alphanumerical sorting provided by the filesystem.
+        Default to True.
+    colour_model : ColourModel
+        A colour model of all images in a test set.
+    """
     class_labels = (
         "fire",
         "nofire",
@@ -64,4 +123,11 @@ def showcase(
     for label in class_labels:
         data_source_directory = test_set_path / label
         image_files = list(data_source_directory.glob("*.jpg"))[:samples_per_class]
-        assess_images(model=model, image_paths=image_files, true_label=label)
+        if randomise_order:
+            shuffle(image_files)
+        assess_images(
+            model=model,
+            image_paths=image_files,
+            true_label=label,
+            colour_model=colour_model,
+        )
